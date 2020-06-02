49a9189a3fbffb47453f94e22cf56c47e481f196
Sherk-d <18856017900@163.com>
2020-06-02 05:14:54 -0700

    update

---
+++ build/intermediates/device/CMakeFiles/Device.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Device.dir/mask_detection_inference/mask_detection_inference.cpp.o"
+  "/home/ascend/AscendProjects/sample-maskdetection/build/outputs/libDevice.pdb"
+  "/home/ascend/AscendProjects/sample-maskdetection/build/outputs/libDevice.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Device.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
